02a7aab6ea7cc34606403a0ce77130b47b47526e
AngelCraft15 <43421778+AngelCraft15@users.noreply.github.com>
2022-06-26 15:28:45 +0200

    Initial commit

---
+++ src/main/java/es/angelillo15/zangeltags/ZAngelTags.java
@@ -0,0 +1,17 @@
+package es.angelillo15.zangeltags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ZAngelTags extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
